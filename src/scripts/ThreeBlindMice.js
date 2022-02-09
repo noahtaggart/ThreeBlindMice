d0d93ecf2367779d9adf5e78cd0175b3c1224258
Noah Taggart <noahtag@gmail.com>
2022-02-09 14:50:51 -0600

    added departments and matched them to their employees

--- src/scripts/ThreeBlindMice.js
+++ src/scripts/ThreeBlindMice.js
@@ -2,7 +2,7 @@ import { EmployeeComputerList } from "./EmployeesList.js";
 
 export const ThreeBlindMice = () => {
     return `
-        <h1>Employee Computer Assignment</h1>
+        <h1>Employee Computer and Department Assignment</h1>
         <section class="employeeComputerList">
         ${EmployeeComputerList()}
         </section>`
