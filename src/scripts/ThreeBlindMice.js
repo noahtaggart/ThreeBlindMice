c5efdeeb01b22d6105d738480fb46517db0cd1f8
Noah Taggart <noahtag@gmail.com>
2022-02-10 10:25:16 -0600

    added customer list with matching employees

--- src/scripts/ThreeBlindMice.js
+++ src/scripts/ThreeBlindMice.js
@@ -1,9 +1,17 @@
+import { CustomerList } from "./CustomerList.js";
 import { EmployeeComputerList } from "./EmployeesList.js";
 
 export const ThreeBlindMice = () => {
     return `
-        <h1>Employee Computer and Department Assignment</h1>
+        <h1>Employee Assignments</h1>
         <section class="employeeComputerList">
         ${EmployeeComputerList()}
-        </section>`
+        </section>
+        <h2>Customer's Employees</h2>
+        <section class="customersEmployees">
+        ${CustomerList()}
+        </section>
+        `
+        
+        
 }
